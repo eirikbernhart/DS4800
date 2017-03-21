ba0c22d1dc9f283ce70b06a31dc8e2273393ed5f
themoen <kristermoen@Kristers-MacBook-Pro.local>
2017-03-21 13:39:54 +0100

    Fjernet about TAB

--- inhouseFoodora/src/pages/tabs/tabs.ts
+++ inhouseFoodora/src/pages/tabs/tabs.ts
@@ -11,7 +11,6 @@ export class TabsPage {
   // this tells the tabs component which Pages
   // should be each tab's root Page
   tab1Root: any = HomePage;
-  tab2Root: any = AboutPage;
   tab3Root: any = ToplistPage;
 
   constructor() {
