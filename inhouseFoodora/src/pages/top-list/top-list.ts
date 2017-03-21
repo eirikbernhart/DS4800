cc96fbe3025dc1cc25674ec39f84ce76d45fbe27
stompalumpa <bereir15@student.westerdals.no>
2017-03-21 12:49:23 +0100

    TopList eksrempel

---
+++ inhouseFoodora/src/pages/top-list/top-list.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+import { ListItem } from '../top-list/list-item';
+
+
+@Component({
+  selector: 'top-list',
+  templateUrl: 'top-list.html'
+})
+export class ToplistPage {
+
+  public topList = [];
+  public testPic = "http://pngimages.net/sites/default/files/ok-png-image-54396.png";
+
+  constructor(public navCtrl: NavController) {
+      this.topList.push(
+        new ListItem("Peppes", 1),
+        new ListItem("Big Horn", 2)
+        );
+  }
+
+}
